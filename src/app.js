3ee765f85f061730c6d27f35dc622a6c381adaba
kbm-sketch <kbretmanna@gmail.com>
2025-07-10 17:54:19 +0000

    Domain Name

--- src/app.js
+++ src/app.js
@@ -7,5 +7,32 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let prefixes = ['awesome', 'super', 'cool','epic']; 
+  let nouns = ['website', 'site', 'domain','developer', 'coder', 'project'];
+  let domains = ['.com', '.net','.org'];
+
+  const domainNames = document.getElementById("domainName")
+
+ 
+
+  for (let i = 0; i < prefixes.length; i++) {
+    for (let j = 0; j < nouns.length; j++) {
+      for (let k = 0; k < domains.length; k++) {
+        let domainName = prefixes[i] + nouns[j] + domains[k];
+          const domainDiv = document.createElement('div');
+                domainDiv.textContent = domainName;
+                domainNames.appendChild(domainDiv);
+       }
+      }
+    }
+
+   
+       
+  
 };
+
+  
+
+
+  
+
